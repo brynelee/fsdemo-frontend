f766cff9b9cb7a1db8125b2866880e5324d9174b
Bryne Lee <bryne_lxd@sina.com>
2020-03-26 00:28:34 +0800

    adjust for fsdemo-authsrv routing.

--- src/router/router.js
+++ src/router/router.js
@@ -8,7 +8,13 @@ import Key from '@/components/key';
 
 Vue.use(VueRouter);
 
+//let authURL = "/oauth/authorize?client_id=fsdemo-frontend&redirect_uri=http://fsdemo-usercenter:8081/usercenter/auth&response_type=code&scope=all&state=fsdemo-frontend";
+//let authURL1 = "http://localhost:8084/oauth/authorize?client_id=fsdemo-frontend&redirect_uri=http://fsdemo-usercenter:8081/usercenter/auth&response_type=code&scope=all&state=fsdemo-frontend";
+
 const router = new VueRouter({
+
+
+
     routes: [
         {
             path: '/',
